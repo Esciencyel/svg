21feaee1639479209e909426958a8a5c815045d0
Esciencyel <152694238+Esciencyel@users.noreply.github.com>
2023-12-26 18:03:15 +0100

    update svgwrite/etree.py

--- svgwrite/etree.py
+++ svgwrite/etree.py
@@ -17,9 +17,8 @@
 
 
 import sys
-PY3 = sys.version_info[0] > 2
-
-import xml.etree.ElementTree as etree
+#import xml.etree.ElementTree as etree
+from lxml import etree
 
 CDATA_TPL = "<![CDATA[%s]]>"
 CDATA_TAG = CDATA_TPL
@@ -32,18 +31,11 @@ def CDATA(text):
 
 original_serialize_xml = etree._serialize_xml
 
-if PY3:
-    def _serialize_xml_with_CDATA_support(write, elem, qnames, namespaces, **kwargs):
+def _serialize_xml_with_CDATA_support(write, elem, qnames, namespaces, **kwargs):
         if elem.tag == CDATA_TAG:
             write(CDATA_TPL % elem.text)
         else:
             original_serialize_xml(write, elem, qnames, namespaces, **kwargs)
-else:
-    def _serialize_xml_with_CDATA_support(write, elem, encoding, qnames, namespaces):
-        if elem.tag == CDATA_TAG:
-            write(CDATA_TPL % elem.text.encode(encoding))
-        else:
-            original_serialize_xml(write, elem, encoding, qnames, namespaces)
 
 # ugly, ugly, ugly patching
 etree._serialize_xml = _serialize_xml_with_CDATA_support
